78cc1f276f349c7f0f460d0f8e5e8f985948b4cd
Semen Kologrivov <semen@sequenia.com>
2024-06-18 11:19:19 +0700

    feat: add scroll to top method to scroll view

--- Sources/Extensions/UIKit/UIScrollView/UIScrollView+SQDevKit.swift
+++ Sources/Extensions/UIKit/UIScrollView/UIScrollView+SQDevKit.swift
@@ -20,4 +20,20 @@ public extension SQExtensions where Base: UIScrollView {
         CGPoint(x: self.base.contentSize.width  - self.base.bounds.width  + self.base.contentInset.right,
                 y: self.base.contentSize.height - self.base.bounds.height + self.base.contentInset.bottom)
     }
+    
+    func scrollToTop(animated: Bool = false) {
+        var topContentOffset: CGPoint
+        if #available(iOS 11.0, *) {
+            topContentOffset = CGPoint(
+                x: -self.base.safeAreaInsets.left,
+                y: -self.base.safeAreaInsets.top
+            )
+        } else {
+            topContentOffset = CGPoint(
+                x: -self.base.contentInset.left,
+                y: -self.base.contentInset.top
+            )
+        }
+            self.base.setContentOffset(topContentOffset, animated: animated)
+        }
 }
