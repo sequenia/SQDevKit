7e898ba410050c047940d2bd14d3f3398ec42256
Semen Kologrivov <semen@sequenia.com>
2025-09-12 20:49:11 +0700

    feat: modify bar button setup

--- Sources/Extensions/UIKit/UIBarButtonItem/UIBarButtonItem+SQDevKit.swift
+++ Sources/Extensions/UIKit/UIBarButtonItem/UIBarButtonItem+SQDevKit.swift
@@ -8,27 +8,36 @@
 import UIKit
 import SnapKit
 
+public enum SQBarSide {
+    case left
+    case right
+}
+
+
 public extension UIBarButtonItem {
     
     convenience init(
         customView: UIView,
-        navigationBar: UINavigationBar
+        navigationBar: UINavigationBar,
+        atSide side: SQBarSide
     ) {
-        if let button = customView as? UIButton,
-           let buttonImage = button.image(for: .normal) {
+        if let button = customView as? UIButton {
             button.snp.makeConstraints {
-                $0.height.width.equalTo(navigationBar.frame.height)
+                $0.height.equalTo(navigationBar.frame.height)
+                $0.width.greaterThanOrEqualTo(navigationBar.frame.height)
             }
 
-            if #available(iOS 15.0, *) {
-                var buttonConfiguration = UIButton.Configuration.plain()
-                buttonConfiguration.contentInsets = .init(
-                    top: .zero,
-                    leading: (navigationBar.frame.height - buttonImage.size.width),
-                    bottom: .zero,
-                    trailing: .zero
-                )
-                button.configuration = buttonConfiguration
+            if let buttonImage = button.image(for: .normal) {
+                if #available(iOS 15.0, *) {
+                    var buttonConfiguration = button.configuration ?? UIButton.Configuration.plain()
+                    buttonConfiguration.contentInsets = .init(
+                        top: .zero,
+                        leading: (navigationBar.frame.height - buttonImage.size.width),
+                        bottom: .zero,
+                        trailing: .zero
+                    )
+                    button.configuration = buttonConfiguration
+                }
             }
         }
         
