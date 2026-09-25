1ed9859cc5633729902c1d52f375e61ffa6c26d1
Semen Kologrivov <semen@sequenia.com>
2026-09-25 18:20:25 +0700

    feat: up min ios version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SQDevKit",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "SQDevKit",
